1046e64544f2a2ebabcd0b150db7b3e7ab818f38
Adrienne Yao <adrienne.yao@icloud.com>
2019-09-27 02:35:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @analyzed_text = TextAnalyzer.new(params[:user_text])
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
 
   erb :results
   end
